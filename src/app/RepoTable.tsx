61c8de5f5cff0b5c7e0a218addeba1886b74ebab
TPewarski <tjp2122@gmail.com>
2024-03-04 21:25:43 -0500

    Responsive styling and type buttons

--- src/app/RepoTable.tsx
+++ src/app/RepoTable.tsx
@@ -6,19 +6,31 @@ interface RepoTableProps {
 }
 export const RepoTable = (props: RepoTableProps) => {
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th scope="col" className="px-6 py-3">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+      <thead className="text-xs text-gray-700 uppercase md:border-none bg-gray-50 border-b-4">
+        <tr className="flex flex-col flex-no wrap md:table-row">
+          <th
+            scope="col"
+            className="px-6 py-3 md:border-none border-b border-dashed"
+          >
             Name
           </th>
-          <th scope="col" className="px-6 py-3">
+          <th
+            scope="col"
+            className="px-6 py-3 md:border-none border-b border-dashed"
+          >
             Description
           </th>
-          <th scope="col" className="px-6 py-3">
+          <th
+            scope="col"
+            className="px-6 py-3 md:border-none border-b border-dashed"
+          >
             Created
           </th>
-          <th scope="col" className="px-6 py-3">
+          <th
+            scope="col"
+            className="px-6 py-3 md:border-none border-b border-dashed"
+          >
             Updated
           </th>
           <th scope="col" className="px-6 py-3">
@@ -30,17 +42,21 @@ export const RepoTable = (props: RepoTableProps) => {
         {props.repos.map((repo) => (
           <tr
             key={repo.id}
-            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            className="bg-white md:border-b border-b-4 flex flex-col flex-no wrap md:table-row"
           >
-            <td className="px-6 py-4">{repo.name}</td>
-            <td className="px-6 py-4">{repo.description}</td>
-            <td className="px-6 py-4">
+            <td className="px-6 py-4 md:border-none border-b border-dashed">
+              {repo.name}
+            </td>
+            <td className="px-6 py-4 md:border-none border-b border-dashed">
+              {repo.description}
+            </td>
+            <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
               {format(parseISO(repo.created_at), "dd-MM-yyyy")}
             </td>
-            <td className="px-6 py-4">
+            <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
               {format(parseISO(repo.updated_at), "dd-MM-yyyy")}
             </td>
-            <td className="px-6 py-4">
+            <td className="px-6 py-4 min-w-48">
               {format(parseISO(repo.pushed_at), "dd-MM-yyyy")}
             </td>
           </tr>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,54 @@
 "use client";
-
-import Image from "next/image";
-import { useForm, SubmitHandler, set } from "react-hook-form";
-import { use, useState } from "react";
-import ReactPaginate from "react-paginate";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useCallback, useState } from "react";
 import { SortDirection, Sort, Type, Repository } from "./types";
 import { RepoTable } from "./RepoTable";
 
 const GITHUB_API = "https://api.github.com";
 
 type Inputs = {
-  user: string;
-  organization: string;
+  name: string;
 };
 
+enum INPUT_TYPE {
+  USER = "user",
+  ORGANIZATION = "organization",
+}
+
 const getGithubURL = (
-  user?: string,
-  organization?: string,
+  inputType: INPUT_TYPE,
+  name: string,
   page: number = 1,
   sortDirection: SortDirection = SortDirection.ASC,
   type: Type = Type.ALL,
   sort: Sort = Sort.FULL_NAME
 ): string =>
-  `${GITHUB_API}/orgs/${organization}/repos?per_page=10&page=${page}&sort=${sort}&direction=${sortDirection}&type=${type}`;
+  `${GITHUB_API}/${
+    inputType === INPUT_TYPE.ORGANIZATION ? "orgs" : "users"
+  }/${name}/repos?per_page=10&page=${page}&sort=${sort}&direction=${sortDirection}&type=${type}`;
 
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
-
   const [isFetching, setIsFetching] = useState(false);
+  const [inputType, setInputType] = useState<INPUT_TYPE>(
+    INPUT_TYPE.ORGANIZATION
+  );
   const [formError, setFormError] = useState<string | null>(null);
   const [repos, setRepos] = useState<Repository[]>([]);
+  const [sortField, setSortField] = useState<Sort>(Sort.FULL_NAME);
+
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onInputTypeChange = () => {
+    setInputType((prev: INPUT_TYPE) =>
+      prev === INPUT_TYPE.USER ? INPUT_TYPE.ORGANIZATION : INPUT_TYPE.USER
+    );
+  };
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     setIsFetching(true);
     setFormError(null);
     try {
-      fetch(getGithubURL(data.user, data.organization, 1))
+      fetch(getGithubURL(inputType, data.name, 1))
         .then((data) => data.json())
         .then((repos) => setRepos(repos));
     } catch (error) {
@@ -59,29 +68,48 @@ export default function Home() {
           htmlFor="organization"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          Organization
+          Search By:
         </label>
-        <input
-          type="text"
-          id="organization"
-          className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Enter a github organization"
-          {...register("organization")}
-        />
+        <div className="flex flex-row">
+          <button
+            type="button"
+            className={`w-36 mr-12 mb-12 ${
+              inputType === INPUT_TYPE.ORGANIZATION
+                ? "bg-blue-500 hover:bg-blue-700 border-blue-700"
+                : "bg-gray-500 hover:bg-gray-700 border-gray-700"
+            } bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
+            onClick={onInputTypeChange}
+          >
+            Organization
+          </button>
+          <button
+            type="button"
+            className={`w-36 mr-12 mb-12 ${
+              inputType === INPUT_TYPE.USER
+                ? "bg-blue-500 hover:bg-blue-700 border-blue-700"
+                : "bg-gray-500 hover:bg-gray-700 border-gray-700"
+            } text-white font-bold py-2 px-4 border  rounded`}
+            onClick={onInputTypeChange}
+          >
+            USER
+          </button>
+        </div>
         <label
-          htmlFor="user"
+          htmlFor="organization"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          User
+          {inputType === INPUT_TYPE.ORGANIZATION ? "Organization" : "User"}
         </label>
         <input
           type="text"
-          id="user"
-          className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Enter a github username"
-          {...register("user")}
+          id="name"
+          className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-1/2 p-2.5"
+          placeholder={`Enter a github ${
+            inputType === INPUT_TYPE.ORGANIZATION ? "organization" : "user"
+          }`}
+          {...register("name")}
         />
-        <button className="mt-5 bg-blue-500 w-48 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <button className="mt-5 bg-blue-500 w-48 hover:bg-blue-700 border border-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
           Submit
         </button>
         {formError && <span className="text-red-500">{formError}</span>}
