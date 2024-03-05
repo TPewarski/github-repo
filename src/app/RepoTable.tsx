f920519220589b3227eb5146ea4126def20a0909
TPewarski <tjp2122@gmail.com>
2024-03-04 22:34:18 -0500

    Add pagination component

--- src/app/RepoTable.tsx
+++ src/app/RepoTable.tsx
@@ -1,75 +1,92 @@
 import { Repository, Sort } from "./types";
 import { format, parseISO } from "date-fns";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 interface RepoTableProps {
   repos: Repository[];
   handleSortChange: (sortField: Sort) => void;
+  handlePageChange: (event: React.ChangeEvent<unknown>, page: number) => void;
+  page: number;
 }
+
 export const RepoTable = (props: RepoTableProps) => {
   return (
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-      <thead className="text-xs text-gray-700 uppercase md:border-none bg-gray-50 border-b-4">
-        <tr className="flex flex-col flex-no wrap md:table-row">
-          <th
-            scope="col"
-            className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
-            onClick={() => props.handleSortChange(Sort.FULL_NAME)}
-          >
-            Name
-          </th>
-          <th
-            scope="col"
-            className="px-6 py-3 md:border-none border-b border-dashed"
-          >
-            Description
-          </th>
-          <th
-            scope="col"
-            className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
-            onClick={() => props.handleSortChange(Sort.CREATED)}
-          >
-            Created
-          </th>
-          <th
-            scope="col"
-            className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
-            onClick={() => props.handleSortChange(Sort.UPDATED)}
-          >
-            Updated
-          </th>
-          <th
-            scope="col"
-            className="hover:cursor-pointer px-6 py-3"
-            onClick={() => props.handleSortChange(Sort.PUSHED)}
-          >
-            Pushed
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.repos.map((repo) => (
-          <tr
-            key={repo.id}
-            className="bg-white md:border-b border-b-4 flex flex-col flex-no wrap md:table-row"
-          >
-            <td className="px-6 py-4 md:border-none border-b border-dashed">
-              {repo.name}
-            </td>
-            <td className="px-6 py-4 md:border-none border-b border-dashed">
-              {repo.description}
-            </td>
-            <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
-              {format(parseISO(repo.created_at), "dd-MM-yyyy")}
-            </td>
-            <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
-              {format(parseISO(repo.updated_at), "dd-MM-yyyy")}
-            </td>
-            <td className="px-6 py-4 min-w-48">
-              {format(parseISO(repo.pushed_at), "dd-MM-yyyy")}
-            </td>
+    <>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 mb-6">
+        <thead className="text-xs text-gray-700 uppercase md:border-none bg-gray-50 border-b-4">
+          <tr className="flex flex-col flex-no wrap md:table-row">
+            <th
+              scope="col"
+              className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
+              onClick={() => props.handleSortChange(Sort.FULL_NAME)}
+            >
+              Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 md:border-none border-b border-dashed"
+            >
+              Description
+            </th>
+            <th
+              scope="col"
+              className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
+              onClick={() => props.handleSortChange(Sort.CREATED)}
+            >
+              Created
+            </th>
+            <th
+              scope="col"
+              className="hover:cursor-pointer px-6 py-3 md:border-none border-b border-dashed"
+              onClick={() => props.handleSortChange(Sort.UPDATED)}
+            >
+              Updated
+            </th>
+            <th
+              scope="col"
+              className="hover:cursor-pointer px-6 py-3"
+              onClick={() => props.handleSortChange(Sort.PUSHED)}
+            >
+              Pushed
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {props.repos.map((repo) => (
+            <tr
+              key={repo.id}
+              className="bg-white md:border-b border-b-4 flex flex-col flex-no wrap md:table-row"
+            >
+              <td className="px-6 py-4 md:border-none border-b border-dashed">
+                {repo.name}
+              </td>
+              <td className="px-6 py-4 md:border-none border-b border-dashed">
+                {repo.description}
+              </td>
+              <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
+                {format(parseISO(repo.created_at), "dd-MM-yyyy")}
+              </td>
+              <td className="px-6 py-4 md:border-none border-b border-dashed min-w-48">
+                {format(parseISO(repo.updated_at), "dd-MM-yyyy")}
+              </td>
+              <td className="px-6 py-4 min-w-48">
+                {format(parseISO(repo.pushed_at), "dd-MM-yyyy")}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Stack spacing={2}>
+        <Pagination
+          count={10}
+          variant="outlined"
+          shape="rounded"
+          onChange={props.handlePageChange}
+          page={props.page}
+        />
+      </Stack>
+    </>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { SortDirection, Sort, Type, Repository } from "./types";
 import { RepoTable } from "./RepoTable";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import { set } from "date-fns";
-import { on } from "events";
 
 const GITHUB_API = "https://api.github.com";
 
@@ -12,13 +13,13 @@ type Inputs = {
   name: string;
 };
 
-enum INPUT_TYPE {
+enum InputType {
   USER = "user",
   ORGANIZATION = "organization",
 }
 
 const getGithubURL = (
-  inputType: INPUT_TYPE,
+  inputType: InputType,
   name: string,
   page: number = 1,
   sort: Sort = Sort.FULL_NAME,
@@ -26,55 +27,66 @@ const getGithubURL = (
   type: Type = Type.ALL
 ): string =>
   `${GITHUB_API}/${
-    inputType === INPUT_TYPE.ORGANIZATION ? "orgs" : "users"
+    inputType === InputType.ORGANIZATION ? "orgs" : "users"
   }/${name}/repos?per_page=10&page=${page}&sort=${sort}&direction=${sortDirection}&type=${type}`;
 
 export default function Home() {
   const [isFetching, setIsFetching] = useState(false);
-  const [inputType, setInputType] = useState<INPUT_TYPE>(
-    INPUT_TYPE.ORGANIZATION
-  );
+  const [inputType, setInputType] = useState<InputType>(InputType.ORGANIZATION);
   const [formError, setFormError] = useState<string | null>(null);
   const [repos, setRepos] = useState<Repository[]>([]);
   const [sortField, setSortField] = useState<Sort>(Sort.FULL_NAME);
   const [sortDirection, setSortDirection] = useState<SortDirection>(
     SortDirection.ASC
   );
+  const [page, setPage] = useState(1);
 
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit, reset } = useForm<Inputs>();
+  useEffect(() => {
+    if (repos.length) {
+      handleSubmit(onSubmit)();
+    }
+  }, [sortField, sortDirection, page]);
 
   const onInputTypeChange = () => {
-    setInputType((prev: INPUT_TYPE) =>
-      prev === INPUT_TYPE.USER ? INPUT_TYPE.ORGANIZATION : INPUT_TYPE.USER
+    setInputType((prev: InputType) =>
+      prev === InputType.USER ? InputType.ORGANIZATION : InputType.USER
     );
+    reset();
   };
 
   const onSubmit: SubmitHandler<Inputs> = useCallback(
     (data) => {
-      console.log("onSubmit", data);
+      console.log("onSubmit", {
+        data,
+        url: getGithubURL(inputType, data.name, page, sortField, sortDirection),
+      });
       setIsFetching(true);
       setFormError(null);
       try {
-        fetch(getGithubURL(inputType, data.name, 1, sortField, sortDirection))
+        fetch(
+          getGithubURL(inputType, data.name, page, sortField, sortDirection)
+        )
           .then((data) => data.json())
-          .then((repos) => setRepos(repos));
+          .then((repos) => setRepos(repos))
+          .then(() => setIsFetching(false));
       } catch (error) {
         setFormError(`Error fetching repos: ${error}`);
         console.error(error);
       }
-      setIsFetching(false);
     },
-    [sortField, sortDirection, inputType]
+    [sortField, sortDirection, inputType, page]
   );
 
   const onSortChange = (col: Sort) => {
-    console.log("onSortChange", col);
     setSortDirection((prev) =>
       prev === SortDirection.ASC ? SortDirection.DESC : SortDirection.ASC
     );
     setSortField(col);
-    handleSubmit(onSubmit)();
   };
+
+  const onChangePage = (event: React.ChangeEvent<unknown>, value: number) =>
+    setPage(value);
 
   return (
     <main className="flex w-full min-h-screen flex-col justify-between p-24">
@@ -92,7 +104,7 @@ export default function Home() {
           <button
             type="button"
             className={`w-36 mr-12 mb-12 ${
-              inputType === INPUT_TYPE.ORGANIZATION
+              inputType === InputType.ORGANIZATION
                 ? "bg-blue-500 hover:bg-blue-700 border-blue-700"
                 : "bg-gray-500 hover:bg-gray-700 border-gray-700"
             } bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
@@ -103,7 +115,7 @@ export default function Home() {
           <button
             type="button"
             className={`w-36 mr-12 mb-12 ${
-              inputType === INPUT_TYPE.USER
+              inputType === InputType.USER
                 ? "bg-blue-500 hover:bg-blue-700 border-blue-700"
                 : "bg-gray-500 hover:bg-gray-700 border-gray-700"
             } text-white font-bold py-2 px-4 border  rounded`}
@@ -116,25 +128,37 @@ export default function Home() {
           htmlFor="organization"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-          {inputType === INPUT_TYPE.ORGANIZATION ? "Organization" : "User"}
+          {inputType === InputType.ORGANIZATION ? "Organization" : "User"}
         </label>
         <input
           type="text"
           id="name"
-          className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:w-1/2 p-2.5"
+          className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full min-w-80 md:w-1/2 p-2.5"
           placeholder={`Enter a github ${
-            inputType === INPUT_TYPE.ORGANIZATION ? "organization" : "user"
+            inputType === InputType.ORGANIZATION ? "organization" : "user"
           }`}
           {...register("name")}
         />
-        <button className="mt-5 bg-blue-500 w-48 hover:bg-blue-700 border border-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Submit
-        </button>
+        <div className="flex flex-row">
+          <button className="mt-5 mr-5 bg-blue-500 w-48 hover:bg-blue-700 border border-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Submit
+          </button>
+          {isFetching && (
+            <Box sx={{ display: "flex" }}>
+              <CircularProgress />
+            </Box>
+          )}
+        </div>
         {formError && <span className="text-red-500">{formError}</span>}
       </form>
-      {repos.length && (
-        <RepoTable repos={repos} handleSortChange={onSortChange} />
-      )}
+      {repos.length ? (
+        <RepoTable
+          repos={repos}
+          handleSortChange={onSortChange}
+          handlePageChange={onChangePage}
+          page={page}
+        />
+      ) : null}
     </main>
   );
 }
